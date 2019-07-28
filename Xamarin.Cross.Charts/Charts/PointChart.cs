f60c8a9d36d8847abeb153fee2f2a7a2a06cb60d
Joseph Nieminen Nixon <nieminen432@gmail.com>
2019-07-28 10:38:31 -0500

    Simplified nuget dependancies on android project;

--- Xamarin.Cross.Charts/Charts/PointChart.cs
+++ Xamarin.Cross.Charts/Charts/PointChart.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Cross.Charts.Enums;
 using Xamarin.Cross.Charts.Extensions;
 
 namespace Xamarin.Cross.Charts.Charts
